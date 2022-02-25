e689a3a501b0943b6d349d32483deb4f6bf0968f
charlie :) <csanford43@gmail.com>
2022-02-25 12:43:20 -0500

    class

--- RoboCat/Src/Main.cpp
+++ RoboCat/Src/Main.cpp
@@ -19,6 +19,20 @@ int main(int argc, const char** argv)
 
 	SocketUtil::StaticInit();
 
+	SocketAddressPtr servAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:9001");
+	SocketAddressPtr myAddress = SocketAddressFactory::CreateIPv4FromString("127.0.0.1:9000");
+
+	UDPSocketPtr udpSocket = SocketUtil::CreateUDPSocket(SocketAddressFamily::INET);
+	udpSocket->Bind(*myAddress);
+
+	
+
+	std::string msg("Hello server :>");
+	if (udpSocket->SendTo(msg.c_str(), msg.length(), *servAddress))
+	{
+		SocketUtil::ReportError("SendTo");
+	}
+
 	SocketUtil::CleanUp();
 
 	return 0;
